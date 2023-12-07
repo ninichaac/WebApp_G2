51ef16294dce2dac74ea2a0e428f30bc5f78e2a0
PATTARAPON BUNCHUAI <6431503044@lamduan.mfu.ac.th>
2023-12-07 14:30:16 +0700

    serve

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
- const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'webapp'
- });
+const con = mysql.createConnection({
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABAE || "webapp",
+});
 
- module.exports = con;
+module.exports = con;
